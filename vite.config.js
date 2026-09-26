ff81bdc2e0fc6c415f5e07ec7949993ee09312b2
Suraj Suryawanshi <surajsuryawanshi2004@gmail.com>
2026-09-26 16:06:29 +0530

    Auto-update: vite.config.js (26/9/2026, 4:06:28 pm)

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy-policy.html'),
+        terms: resolve(__dirname, 'terms-and-conditions.html')
+      }
+    }
+  }
+});
